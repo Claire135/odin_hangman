a024b6cce28b0756b9a4211d62ded3e9108a227e
Claire135 <186298887+Claire135@users.noreply.github.com>
2025-03-07 18:50:39 +0100

    This has been refactored as a class and logic from computer class has been moved into it. It is now responsible for storing game variables and checking state based on rules.

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,8 +1,43 @@
-module GameLogic
+class GameLogic
+  attr_accessor :historic_guesses, :guesses_remaining
+  attr_reader :word, :discarded_letters, :word_strip
 
-  def is_letter_in_word?(word, guess)
-    word.index(guess)
+  include Dictionary
+
+  def initialize
+    @word = dictionary_array.sample
+    @guesses_remaining = 10
+    @historic_guesses = []
+    @discarded_letters = []
+    @word_strip = Array.new(@word.length, '_')
   end
+
+  def generate_word_strip(guess)
+    match = letter_in_word_index(guess)
+    match.is_a?(Integer) ? @word_strip[match] = guess : @discarded_letters << guess
+    @word_strip
+  end
+
+  def add_historic_guesses(guess)
+    @historic_guesses << guess
+  end
+
+  def check_win
+    !@word_strip.include?('_')
+  end
+  
+  def decrease_guess_count
+    @guesses_remaining -= 1
+  end
+
+  def generate_discarded_letters(guess)
+    if disclarded_letter = letter_in_word_index(word, guess).is_nil?
+      @discarded_letters << disclarded_letter
+    end
+  end
+
+  def letter_in_word_index(guess)
+    @word.index(guess) || []
+  end
+
 end
-  
-  
