8b17d438b9d285f5e00e499c19c179ae30e15e66
Claire135 <186298887+Claire135@users.noreply.github.com>
2025-03-07 18:53:00 +0100

    historic guesses have been moved to GameLogic and errors have been rewritten accordingly

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -8,17 +8,33 @@ class HumanPlayer
     @guess = nil
   end
 
-  def get_player_guess
+  def player_guess_prompt(historic_guesses)
     loop do
-      puts "Guess a letter!"
+      puts 'Guess a letter!'
       @guess = gets.chomp.downcase
-      return @guess if ('a'..'z').include?(@guess)
-      raise StandardError, 'Please enter a valid letter (in the English alphabet!).' unless ('a'..'z').include?(@guess)
-      break
-    rescue StandardError => e
-      puts e.message
-      retry
+  
+      unless valid_guess?
+        puts 'Please enter a valid letter (in the English alphabet!).'
+        next
+      end
+  
+      if already_guessed?(historic_guesses)
+        puts "You've already guessed that letter, pick another one!"
+        next
+      end
+  
+      return @guess
     end
   end
-
+  
+  private
+  
+  def valid_guess?
+    ('a'..'z').include?(@guess)
+  end
+  
+  def already_guessed?(historic_guesses)
+    historic_guesses ||= []
+    historic_guesses.include?(@guess)
+  end
 end
