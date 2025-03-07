5aeff16290e8aae49c0290413de6c62a811e3381
Claire135 <186298887+Claire135@users.noreply.github.com>
2025-03-07 18:54:03 +0100

    game is now looped and the logic is largely functioning correctly. Debugging still in place.

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -1,23 +1,39 @@
 require_relative 'human_player'
 require_relative 'dictionary'
-require_relative 'computer_player'
 require_relative 'game_logic'
+require_relative 'displayable'
 
 class PlayGame
-  include GameLogic
+  include Displayable
 
   def initialize
     @human = HumanPlayer.new
-    @computer = ComputerPlayer.new
+    @logic = GameLogic.new
   end
 
   def play
-    random_word = @computer.generate_random_word
-    p "#{random_word}"
-    @human.get_player_guess
-    match = is_letter_in_word?(@computer.word, @human.guess)
-    p match
+    until @logic.guesses_remaining == 0 || @logic.check_win
+      loop do
+        p "DEBUG #{@logic.word}"
+        @human.player_guess_prompt(@logic.historic_guesses)
+        @logic.generate_word_strip(@human.guess)
+        adjust_counters
+        p @logic.guesses_remaining
+        game_display(@logic.word_strip, @logic.discarded_letters)
+      end
+      end_game
+    end
   end
+
+  def adjust_counters
+    @logic.decrease_guess_count
+    @logic.add_historic_guesses(@human.guess)
+  end
+
+  def end_game
+    if @logic.guesses_remaining == 0
+      lose_game_ui
+    else win_game_ui
 end
 
 g = PlayGame.new
