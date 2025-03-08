cb69e65bc9773d6b582a9608c4e305e3938e1b13
Claire135 <186298887+Claire135@users.noreply.github.com>
2025-03-08 15:21:04 +0100

    display updated to include hangman

--- lib/displayable.rb
+++ lib/displayable.rb
@@ -1,23 +1,26 @@
 module Displayable
+  include HangmanGraphic
 
-  def game_display(word_strip, discarded_letters)
-    display_word_strip(word_strip)
+  def game_display(word_strip, discarded_letters, guesses_remaining)
     display_discarded_letters(discarded_letters)
+    unfurl_hangman(guesses_remaining)
+    display_word_strip(word_strip)
   end
 
   def display_word_strip(word_strip)
     puts word_strip.join(" ")
+    puts("")
   end
 
   def display_discarded_letters(discarded_letters)
-    puts "Incorrect letters: #{discarded_letters.join(", ")}"
+    puts "Incorrect letters: #{discarded_letters.sort.join(", ")}"
   end
 
   def win_game_ui
-    puts "Well done you guess the word! :D "
+    puts "Well done you guessed the word! :D "
   end
 
   def lose_game_ui
     puts "No luck this time! :("
-
+  end
 end
